c1a19cd78366be9c041d0f888c60583e98c960a0
Aleksandr Perekhodko <aleksandrperekhodko@gmail.com>
2022-04-17 02:46:02 -0400

    Adding Challange Query and Challange Data.

--- Analysis Project Folder/Pewlett-Hackard-Analyisis Folder/Queries/Employee_Challenge_starter_code.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analyisis Folder/Queries/Employee_Challenge_starter_code.sql
@@ -1,10 +1,51 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Deliverable 1
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- Retiree Title Table
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Unique Title Table
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no, to_date DESC;
+
+-- Retiring Titles Table
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--Deliverable 2
+
+-- Mentorship Eligibility Table
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+   	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left Outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left Outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
